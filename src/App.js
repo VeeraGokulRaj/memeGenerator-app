ad5dde493909fb26b06feb13bd7d5a1400d4ed58
VeeraGokulRaj <easwarigokul@gmail.com>
2024-02-06 11:42:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar'
+import Meme from './Components/Meme'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Meme/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <img src="./Images/Logo.png" alt="Logo" className="logo" />
+            <p className="nav-content">React Course - Project 3</p>
+        </div>
+    )
+}
